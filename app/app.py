958f2f6c2a8424deca6531b3f4b12849ffd30d66
Paulo Santiago <p.santiago796@gmail.com>
2026-01-27 17:47:03 -0500

    add ticket and user db models

--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, request, jsonify, make_response
+from database import db
+from models import Ticket, User
 
 app = Flask(__name__)
+
+# db configured with sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project6.db'
+
+db.init_app(app)
+
+# create all tables
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# get all tickets
+@app.route('/tickets', methods=['GET'])
+def get_tickets():
+    tickets = Ticket.query.all()
+    # return all tickets as a json
+    return jsonify([t.to_dict() for t in tickets]), 200
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7500, debug=True)
---
+++ app/models/ticket.py
@@ -0,0 +1,36 @@
+from database import db
+from datetime import datetime
+
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+
+    ticketID = db.Column(db.Integer, primary_key=True)
+    employeeID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+    technicianID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    priority = db.Column(db.Enum('Low', 'Medium', 'High', 'Critical', name='ticket_priority'), default='Medium')
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Large Binary = Blob
+    image = db.Column(db.LargeBinary, nullable=True)
+
+    isAssigned = db.Column(db.Boolean, default=False)
+
+    # relationship helpers
+    employee = db.relationship('User', foreign_keys=[employeeID], backref='created_tickets')
+    technician = db.relationship('User', foreign_keys=[technicianID], backref='assigned_tickets')
+
+    def to_dict(self):
+        return {
+            "ticketID": self.ticketID,
+            "employeeID": self.employeeID,
+            "technicianID": self.technicianID,
+            "title": self.title,
+            "description": self.description,
+            "priority": self.priority,
+            "isAssigned": self.isAssigned,
+            "created_at": self.created_at.isoformat() if self.created_at else None
+        }
